694e4e4fd7c5cf2746eab1738058db99b4db8c1e
jfosnc <jfoster@haddonhillgroup.com>
2018-07-03 12:06:32 -0400

    I changed some stuff again

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -2,6 +2,6 @@ package oata;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        System.out.println("Muh Sample App!");
     }
 }
